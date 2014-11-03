66f598ca7059205bc36c63c78bd39fb8730a14cb
刘卿 <csssnow@gmail.com>
2014-11-03 16:17:10 +0800

    Update detectZoom.js
    fixed： 新版chrome，缩放不准确

--- detectZoom.js
+++ detectZoom.js
@@ -15,25 +15,6 @@
             return 'opera';
         };
     }
-    function checkMobile(){
-        var pda_user_agent_list = ['iPhone','Android','Symbian OS','Windows','BlackBerry','Opera Mobi','iPad','iPod','Windows Phone','HTC','SonyEricsson','Nokia'];
-        var pda_app_name_list =['Microsoft Pocket Internet Explorer'];
-
-        var user_agent = navigator.userAgent.toString();        
-        for (var i=0; i<pda_user_agent_list.length; i++) {
-            if (user_agent.indexOf(pda_user_agent_list[i]) >= 0) {
-                return true;
-            }
-        }
-        var appName = navigator.appName.toString();
-        for (var i=0; i<pda_app_name_list.length; i++) {
-            if (user_agent.indexOf(pda_app_name_list[i]) >= 0) {
-                return true;
-            }
-        }
-
-        return false;
-    }
     /* 设备检测
     *  继续探索一种不使用mediaQuery的方法，求推荐
     */
@@ -79,70 +60,36 @@
             }
         }
     };
-    /*
-    * 非完美解决恢复正常浏览器缩放比例
-    * ff不支持zoom，所以采用css3的transform来进行缩放
-    * transform-origin是用来改变位置
-    */
-    function reZoom(x){
-        var zoom = Math.round(100/(x/100));
-        switch(user_agent()){
-            case 'firefox':
-                var style = document.createElement('style');
-                style.innerHTML = 'body {transform: scale('+zoom/100+');-moz-transform-origin:top left;}';
-                document.body.appendChild(style);
-                break;
-            default:
-                document.body.style.zoom = zoom +'%';
-        }
+    var zoom = {}
+    zoom.ie = function(){
+        return window.screen.deviceXDPI / window.screen.logicalXDPI;
     }
+
+    zoom.firefox = function(){
+        return mediaQueryBinarySearch('min--moz-device-pixel-ratio', '', 0, 10, 20, 0.0001);
+    }
+
+    zoom.opera = function(){
+        return window.outerWidth / window.innerWidth;   
+    }
+
+    zoom.chrome = function(){
+        if(window.devicePixelRatio) {
+            return window.devicePixelRatio;
+        }
+        var div = document.createElement('div');
+        div.innerHTML = '1';
+        div.setAttribute('style','font:100px/1em sans-serif;-webkit-text-size-adjust:none;position: absolute;top:-100%;');
+        document.body.appendChild(div);
+        var zoom = 1000 / div.clientHeight;
+        zoom = Math.round(zoom * 100) / 100;
+        document.body.removeChild(div);
+        return zoom;
+    }
+
     //api
     window.detectZoom = function(){
-        if (!checkMobile()) {
-            return;
-        };
-        var zoom = 100;
-        switch(user_agent()){
-            case 'ie':
-                zoom = window.screen.deviceXDPI / window.screen.logicalXDPI;
-                break;
-            case 'firefox':
-                // 检测设备缩放比例
-                zoom = mediaQueryBinarySearch('min--moz-device-pixel-ratio', '', 0, 10, 20, 0.0001);
-                break;
-            case 'opera':
-                zoom = window.outerWidth / window.innerWidth;
-                break;
-            default :
-                //新版chrome浏览器支持devicePixelRatio，旧版可以使用 webkit-text-size-adjust来做
-                //devicePixelRatio是设备上物理像素和设备独立像素
-                //
-                if(window.devicePixelRatio) {
-                    var dpr = window.devicePixelRatio;
-                    zoom = (window.outerWidth*dpr-8)/window.innerWidth;
-                }else{
-                    var tempElem = document.createElement('div');
-                    tempElem.innerHTML = '1';
-                    tempElem.setAttribute('style','font:100px/1em sans-serif;-webkit-text-size-adjust:none;position: absolute;top:-100%;');
-                    document.body.appendChild(tempElem);
-                    var x = 100/tempElem.clientHeight;
-                }
-        }
-        zoom = Math.round(zoom * 100);
-        var zoomTips = document.createElement('div');
-        zoomTips.innerHTML = '您当前的浏览器缩放比例为'+ zoom + "%";
-        document.body.appendChild(zoomTips);
-        /**
-         * 如果缩放比例不合适，可以通过zoom/scale来模拟
-         */
-        if (zoom!=100) {
-            var div = document.createElement('div');
-            div.innerHTML = '<button id="reZoom">点击恢复100%</button>';
-            document.body.appendChild(div);
-            document.getElementById('reZoom').onclick = function(){
-                reZoom(zoom);
-            }
-        };
+        return zoom[user_agent()]();
     }    
 
 }(this);
